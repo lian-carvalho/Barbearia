d74380d75003b35a298e5d86fe54a302fe2364f6
Lian Dev <liancarvalhodacosta@gmail.com>
2026-02-04 20:34:20 -0300

    Configuração de layout

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import '../styles/globals.css';
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Barbearia",
+  description: "A portfolio project developed by Lian Dev",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
