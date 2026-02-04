6dd13667d1864dd8857734882b8bb34124380bb0
Lian Dev <liancarvalhodacosta@gmail.com>
2026-02-04 20:36:15 -0300

    Adição a página inicial

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/navigation/Header";
+import HeroSection from "@/components/sections/HeroSection";
+import AboutUsSection from "@/components/sections/AboutUsSection";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <AboutUsSection />
+    </div>
+  );
+}
