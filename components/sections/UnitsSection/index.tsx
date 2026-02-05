255b7808f835695091e4c203d091534b3849656f
Lian Dev <liancarvalhodacosta@gmail.com>
2026-02-05 18:49:40 -0300

    UnitsSection

---
+++ components/sections/UnitsSection/index.tsx
@@ -0,0 +1,34 @@
+import styles from './style.module.css'
+
+import UnitCard from '@/components/renders/UnitCard'
+
+export default function UnitsSection() {
+
+    const unidades = [
+        {
+            rua: 'Rua Aguiar, n120',
+            cidade: 'Campo Grande - MS',
+            imageUrl: 'unidade-aguiar.png',
+        },
+        {
+            rua: 'Rua Centro, n10',
+            cidade: 'Campo Grande - MS',
+            imageUrl: 'unidade-centro.png',
+        },
+    ]
+
+    return (
+        <section className={styles.units}>
+            <h1 className="c-blue font-bold">NOSSAS UNIDADES</h1>
+            <div className={styles.container}>
+                {unidades.map((unit, index) => (
+                    <UnitCard
+                        key={index}
+                        rua={unit.rua}
+                        cidade={unit.cidade}
+                        imgUrl={unit.imageUrl} />
+                ))}
+            </div>
+        </section>
+    )
+}
