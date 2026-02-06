c40992f770e68031ef326e91b756638006a4c3a2
Lian Dev <liancarvalhodacosta@gmail.com>
2026-02-06 20:25:30 -0300

    HeroSection com openModal

--- components/sections/HeroSection/index.tsx
+++ components/sections/HeroSection/index.tsx
@@ -1,12 +1,16 @@
 import styles from './style.module.css'
 
-export default function HeroSection () {
-    return(
+type HeroSectionProps = {
+    openModalFunction: () => void;
+};
+
+export default function HeroSection({ openModalFunction }: HeroSectionProps) {
+    return (
         <section className={styles.hero} id='hero'>
             <div>
                 <h1 className="font-bold c-white">ESTILO É UM REFLEXO DA SUA ATITUDE E SUA PERSONA.</h1>
                 <h5 className="c-white">Horário de funcionamento: 09:00 ás 18:00</h5>
-                <button className={styles.agendBtn}>Agendar horário</button>
+                <button className={styles.agendBtn} onClick={() => { openModalFunction() }}>Agendar horário</button>
             </div>
         </section>
     )
