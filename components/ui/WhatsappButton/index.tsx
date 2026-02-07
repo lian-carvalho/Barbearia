85d17e5f5d7277701748aec27378a990f9d22457
Lian Dev <liancarvalhodacosta@gmail.com>
2026-02-07 17:44:37 -0300

    Adição de link para whatsapp em WhatsappButton

--- components/ui/WhatsappButton/index.tsx
+++ components/ui/WhatsappButton/index.tsx
@@ -3,8 +3,13 @@ import styles from './style.module.css'
 import Image from 'next/image'
 
 export default function WhatsappButton() {
+
+    const phone = '5588981424288';
+    const message = 'Olá, vi o seu ótimo trabalho com o site da barbearia e gostaria de encomendar o meu próprio!';
+    const whatsappLink = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
     return (
-        <a href='' className={styles.whatsBtn}>
+        <a href={whatsappLink} className={styles.whatsBtn} target="_blank" rel="noopener noreferrer" title='Encomende o seu site agora mesmo'>
             <div>
                 <Image
                     fill
